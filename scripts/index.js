a942d6a4869d4571792d816b8e3e0bf398cc0d40
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-10-12 15:24:59 -0400

    adds create image function that sets the URL from the argument

--- scripts/index.js
+++ scripts/index.js
@@ -4,3 +4,14 @@ const IMAGES = [
     "https://www.pixel-creation.com/wp-content/uploads/houston-texans-computer-wallpaper-52917-1600x900-px-hdwallsource-800x800.jpg",
     "http://ava7.com/w/basketball-teams/houston-rockets/houston-rockets-nba-basketball-team.jpg"
 ]
+
+// function that generates the thumbnail div
+// function that generates an imag element
+function createImage(imageURL) {
+    const theImage = document.createElement(`img`);
+
+    theImage.src =imageURL;
+    // theImage.setAttribute('src', imageURL);
+
+    return theImage;
+}
