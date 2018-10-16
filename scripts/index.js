c4c514e88750337e8ecfe3d6d2739344acad8980
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-10-16 09:52:38 -0400

    completes get previous and next images

--- scripts/index.js
+++ scripts/index.js
@@ -4,6 +4,45 @@ const IMAGES = [
     "https://www.pixel-creation.com/wp-content/uploads/houston-texans-computer-wallpaper-52917-1600x900-px-hdwallsource-800x800.jpg",
     "http://ava7.com/w/basketball-teams/houston-rockets/houston-rockets-nba-basketball-team.jpg"
 ]
+
+// Array Navigation functions
+function getNextImage(currentURL) {
+    // find the currentURL's index in the IMAGES array
+    let index = IMAGES.indexOf(currentURL);
+    // TODO: check if index is -1 at this point
+    // show an error or do something nice.
+
+    // increment the index
+    index++;
+
+    // check if its within bounds, reset if necessary
+    if (index === IMAGES.length) {
+        index = 0;
+    }
+
+    // then return the images URL at the new index
+    return IMAGES[index];
+}
+
+function getPrevImage(currentURL) {
+    // find the currentURL's index in the IMAGES array
+    let index = IMAGES.indexOf(currentURL);
+    // TODO: check if index is -1 at this point
+    // show an error or do something nice.
+
+    // decrement the index
+    index--;
+
+    // check if its within bounds, reset if necessary
+    if (index < 0) {
+        index = IMAGES.length - 1;
+    }
+
+    // then return the images URL at the new index
+    return IMAGES[index];
+}
+
+// Dom element entries
 const thumbnailContainer = document.querySelector('[data-container]');
 const outputElement = document.querySelector('[data-output]');
 const modalElement = document.querySelector('[data-modal]');
@@ -35,7 +74,7 @@ function createImage(imageURL) {
         modalElement.classList.remove(`modal-hidden`);
         
     });
-
+    
     return theImage;
 }
 
@@ -44,38 +83,69 @@ function createThumbnail(imageURL) {
     const theContainer = document.createElement('div');
     theContainer.classList.add(`thumbnail-item`);
     theContainer.appendChild(createImage(imageURL));
-
+    
     return theContainer;
 }
 
 // MY ATTEMPT:
 // function drawThumbnail() {
-//     let firstImageURL = IMAGES[index];
-//     let testThumb = createThumbnail(firstImageURL);
-//     document.body.appendChild(testThumb);
-//     index ++;
-// }
-
-// CHRIS's walkthrough:
-// loop through the IMAGES array
-// for each image, call the anonymous function
-// the anon func should expect to receive an image URL
-IMAGES.forEach(function (anImageUrl) {
-
-    // We pass that image URL ro our createThumbnail func
-    let aThumbnail = createThumbnail(anImageUrl);
-
-    // then append that thumbnail to the page
-    thumbnailContainer.appendChild(aThumbnail);
-});
-
-window.addEventListener('keydown', function () {
+    //     let firstImageURL = IMAGES[index];
+    //     let testThumb = createThumbnail(firstImageURL);
+    //     document.body.appendChild(testThumb);
+    //     index ++;
+    // }
+    
+    // CHRIS's walkthrough:
+    // loop through the IMAGES array
+    // for each image, call the anonymous function
+    // the anon func should expect to receive an image URL
+    IMAGES.forEach(function (anImageUrl) {
+        
+        // We pass that image URL ro our createThumbnail func
+        let aThumbnail = createThumbnail(anImageUrl);
+        
+        // then append that thumbnail to the page
+        thumbnailContainer.appendChild(aThumbnail);
+    });
+    
+    
+    // // add left and right keyboard clicks to go between images --------------------------------------------------------------
+    // window.addEventListener('keydown', function () {
+        //     // console.log(`you pressed a key`);
+        //     console.log(event);
+        //     // key: "ArrowLeft"
+        //     // keyCode: 37
+        //     // key: "ArrowRight"
+        //     // keyCode: 39
+        //     if (event.keyCode === 37) {
+            //         this.console.log(`this should go left`);
+            //     if (event.keyCode === 39) {
+                //         this.console.log(`this should go right`);
+                //         // modalElement.classList.add(`modal-hidden`);
+                //     }
+                // });
+window.addEventListener('keydown', function (event) {
     // console.log(`you pressed a key`);
     // console.log(event);
     // key: "Escape"
     // keyCode: 27
     if (event.keyCode === 27) {
-        this.console.log(`i want to hide the modal`);
+        console.log(`i want to hide the modal`);
         modalElement.classList.add(`modal-hidden`);
+    }
+});
+// have it so when you click the image goes away
+window.addEventListener('click', function (event) {
+    if (event.target == outputElement) {
+        modalElement.classList.add(`modal-hidden`);
+    }
+});
+  // Add global previous/next keyboard listeners
+window.addEventListener(`keydown`, function (event) {
+    console.log(event.keyCode);
+    if (event.keyCode === 37) {
+        console.log(`go to previous image`);
+    } else if (event.keyCode === 39) {
+        console.log(`go to the next image`);
     }
 });
